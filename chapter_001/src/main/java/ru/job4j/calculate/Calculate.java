e96c2bf9934c2f3fe01c634e31494a70d51f53b8
AlexDogaev <alexdogaev@gmail.com>
2019-04-15 00:36:30 +0300

    1.3. JavaDoc. Code convention[#132751]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,24 @@
 package ru.job4j.calculate;
 
+/**
+* Class Класс для вычисления арифметических операций + - * / ^
+* @author alexdogaev
+* @since 15.04.2019
+* @version 1
+*/
 public class Calculate {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+
+	/**
+	* Сложение
+	* @param first первй аргумент
+	* @param second второй аргумент
+	* @return результат
+	*/
+	
+
 	
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Alex Dogaev (alexdogaev@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
+
